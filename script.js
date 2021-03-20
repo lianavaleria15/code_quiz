94ff7e05659ae25a5aed37f0fd34992ac8e742e4
liana valeria <liana.valeria15@gmail.com>
2021-03-20 16:51:35 +0000

    declare start quiz and countadown variables in javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+// declare variables to target buttons in html file
+const startQuizButton = document.getElementById("start-quiz-button");
+const timerQuiz = document.getElementById("timer");
+/* declare function to start quiz: remove info-section, start timer(60secs)*/
+document.addEventListener("click", startQuizButton);
+/* create countdown function
