30f9e3f8a30381a78e55c8524dfcbf7d244a0135
liana valeria <liana.valeria15@gmail.com>
2021-03-20 20:02:33 +0000

    add event listener to start quiz button and create timer function

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // declare variables to target buttons in html file
-const startQuizButton = document.getElementById("start-quiz-button");
+// const startQuizButton = document.getElementById("start-quiz-button");
 const timerQuiz = document.getElementById("timer");
-/* declare function to start quiz: remove info-section, start timer(60secs)*/
-document.addEventListener("click", startQuizButton);
-/* create countdown function
+const startQuizButton = document.getElementById("start_quiz_button");
+
+//initialise counter value
+let timeValue = 60;
+
+/* create countdown function*/
+function startQuiz() {
+  const callback = function () {
+    if (timeValue >= 0) {
+      timerQuiz.textContent = timeValue;
+      timeValue -= 1;
+    }
+  };
+  // function executed every time a button is clicked
+  const timeAnswerQuestion = setInterval(callback, 1000);
+}
+
+// add event listener to start the quiz and timer
+startQuizButton.addEventListener("click", startQuiz);
+//store my questions in arrays
+// when users presses right answer, change question, vAriable that stores
