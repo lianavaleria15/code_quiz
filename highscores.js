a37b2947e6d30d9decf930a21a0b972bd590ccf4
liana valeria <liana.valeria15@gmail.com>
2021-03-28 23:00:07 +0100

    add render highscore and init function

--- highscores.js
+++ highscores.js
@@ -1,6 +1,6 @@
 const goBackButton = document.getElementById("go-back");
 const clearHighScoresButton = document.getElementById("clear-highscores");
-
+const higherScoresList = document.getElementById("higherScoresList");
 const goBack = () => {
   location.href = "./index.html";
 };
@@ -11,23 +11,39 @@ const clearHighScores = () => {
   onLoad();
 };
 
-//function to read from local storage and store in a declared variable
-const getFromLocalStorage = () => {
-  const highestscore = localStorage.getItem("highest-score");
-  if (highestscore) {
-    return highestscore;
-  } else {
-    return [];
-  }
-};
+//initialise array to display highest scores
+const highestScores = [];
 
 //here add scores from local storage into html file TO DO (GET INTO HTML, TRANSLATE INTO JAVASCRIPT, take final div and append to the page)
-const renderHighScores = (highestscore) => {
-  if (highestscore) {
-    console.log("empty"); //display empty table
-  } else {
-    console.log("create table");
+const renderHighScores = () => {
+  //sets higher scores list to an empty string
+  higherScoresList.innerHTML = "";
+  const createScoreItem = (highestScore, index) => {
+    const highestScore = highestScores[i];
+
+    //create a li item and set its value to the value of highest score from local storage
+    const li = document.createElement("li");
+    li.textContent = highestScore;
+    li.setAttribute("data-index", index);
+    higherScoresList.appendChild(li);
+  };
+  highestScores.forEach(createScoreItem);
+};
+
+//function to read from local storage and store in a declared variable
+const getFromLocalStorage = () => {
+  // checks if there is anything in local storage with the value of highest-score
+  const storedHighScores = localStorage.getItem("highest-score");
+  if (storedHighScores !== null) {
+    highestScores.push(storedHighScores);
   }
+  renderHighScores();
+};
+
+const init = () => {
+  //
+  const highestScores = localStorage.getItem("highest-score");
+  renderHighScores();
 };
 
 // here function to load scores
